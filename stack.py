3a463ac50e5bd9c5f89ef41a5c6eef291b538bb6
James Sathre <jamessathre@gmail.com>
2026-05-06 10:53:13 -0700

    removed comments section added double line spaces between class definitions and main function

--- stack.py
+++ stack.py
@@ -11,6 +11,7 @@ Description: This script is an example stack implementation
 
 import unittest
 
+
 class Stack:
 
     def __init__(self):
@@ -39,6 +40,7 @@ class Stack:
     def size(self):
         """returns the number of elements in the stack"""
         return len(self.data)
+
 
 class TestStackMethods(unittest.TestCase):
 
@@ -77,20 +79,6 @@ class TestStackMethods(unittest.TestCase):
         stack_obj.push(2)
         stack_obj.push(3)
         self.assertTrue(stack_obj.size() == 3)
-
-    # def test_upper(self):
-    #     self.assertEqual('foo'.upper(), 'FOO')
-
-    # def test_isupper(self):
-    #     self.assertTrue('FOO'.isupper())
-    #     self.assertFalse('Foo'.isupper())
-
-    # def test_split(self):
-    #     s = 'hello world'
-    #     self.assertEqual(s.split(), ['hello', 'world'])
-    #     # check that s.split fails when the separator is not a string
-    #     with self.assertRaises(TypeError):
-    #         s.split(2)
 
 
 if __name__ == '__main__':
